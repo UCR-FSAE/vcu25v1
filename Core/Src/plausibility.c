58d01a8a09754a9d73b727f834a02a442e207992
SRaionido <sleon027@ucr.edu>
2025-06-05 23:05:29 -0700

    Adding in code for possible implementation of reading Accelerator Pedal Position percentage.

--- Core/Src/plausibility.c
+++ Core/Src/plausibility.c
@@ -7,6 +7,14 @@
 
 #include "plausibility.h"
 
+extern uint32_t appsRaw1Min;
+extern uint32_t appsRaw1Max;
+extern uint32_t appsRaw2Min;
+extern uint32_t appsRaw2Max;
+
+static uint32_t appsRaw1 = 0;
+static uint32_t appsRaw2 = 0;
+
 /*
  * AccelPos:
  * Get Accelerator Potentiometer values from ADC and determine position percentage
@@ -15,8 +23,32 @@
 float AccelPos() {
 	// TO BE IMPLEMENTED
 
+	if (HAL_ADC_PollForConversion(&hadc3, 5) == HAL_OK) {
+		appsRaw1 = HAL_ADC_GetValue(&hadc3);
+	}
+	else {
+		// throw fault
+	}
+	if (HAL_ADC_PollForConversion(&hadc3, 7) == HAL_OK) {
+		appsRaw2 = HAL_ADC_GetValue(&hadc3);
+	}
+	else {
+		// throw fault
+	}
+
+	// FIGURE OUT HOW TO CALCULATE PERCENTAGE BASED ON LIMITS
+	// PLACEHOLDER, Unsure if works yet
+	uint32_t app1Range = appsRaw1Max - apps2Min;
+	uint32_t app2Range = appsRaw2Max - apps2Min;
+
+	float percent1 = appsRaw1 / apps1Range;
+	float percent2 = appsRaw2 / apps2Range;
+
+	float percentAvg = (percent1 + percent2) / 2;
+
+
 	// PLACEHOLDER
-	return 0.5;
+	return percentAvg;
 
 }
 
@@ -50,6 +82,8 @@ int PlausibilityCheck(float accel, float brake) {
  * If Pass, Map torque request based on Accelerator Pedal Position and return torque request value
  */
 int MapTorque() {
+	HAL_ADC_Start(&hadc3);
+
 	float accel = AccelPos();
 	float brake = BrakePos();
 
