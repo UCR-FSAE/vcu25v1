5bb91dcf7e5d5807a68266fb96effaff08f7fd32
justin-h-im <imjustin679@gmail.com>
2025-06-20 11:47:51 -0400

    added gpio output for buzzer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -845,6 +845,9 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOG_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
@@ -857,6 +860,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PE5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pin : USER_Btn_Pin */
@@ -985,9 +995,9 @@ void PedalCalStart(void *argument)
 {
   /* USER CODE BEGIN PedalCalStart */
 	appsCalibrate();
-//	osDelay(1);
+
 	brakeCalibrate();
-//	osDelay(1);
+	osDelay(1);
 	vTaskDelete(NULL);
   /* USER CODE END PedalCalStart */
 }
